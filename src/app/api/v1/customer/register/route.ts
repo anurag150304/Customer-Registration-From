c0de6bcff6dd8a5331c32ffcb6084c69da9504ea
Anurag Mishra <anurag.mishra150304@gmail.com>
2025-07-27 02:31:53 +0530

    added validation

--- src/app/api/v1/customer/register/route.ts
+++ src/app/api/v1/customer/register/route.ts
@@ -12,9 +12,14 @@ export async function POST(req: NextRequest) {
 
     try {
 
-        const customerExists = await dbClient.customer.findFirst({ where: { email: data.email, phone: data.phone } });
-        if (customerExists) {
-            throw new errHandler("Customer with this email or phone already exists.", 400);
+        const customerExistsWithEmail = await dbClient.customer.findFirst({ where: { email: data.email } });
+        if (customerExistsWithEmail) {
+            throw new errHandler("Customer with this email already exists.", 400);
+        }
+
+        const customerExistsWithPhone = await dbClient.customer.findFirst({ where: { phone: data.phone } });
+        if (customerExistsWithPhone) {
+            throw new errHandler("Customer with this phone already exists.", 400);
         }
         const hashedPassword = await bcrypt.hash(data.password, 10);
 
